434c53a4ff79fdd2d4c35f86db1b496c88592bcb
cmaguire27 <c.maguire19@nuigalway.ie>
2021-04-07 10:10:56 +0100

    Bones for Cap Demo added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {useRef,useState}from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Canvas, useFrame} from 'react-three-fiber';
+import {OrbitControls} from '@react-three/drei';
+
+function Box(props){
+  //Provide direct access to the mesh
+  const mesh = useRef();
+
+  //Set up state for hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  //Rotate mesh
+  useFrame(()=> {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  });
+
+  return(
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5,1.5,1.5] : [1,1,1]}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+        <boxBufferGeometry args={[1,1,1]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+  );
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <Canvas>
+   <OrbitControls />
+   <ambientLight />
+   <pointLight position={[10,10,10]} />
+   <Box position={[-1.2,0,0]} />
+   <Box position={[1.2,0,0]} />
+  </Canvas>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
